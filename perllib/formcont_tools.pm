epfl-si/payonline

commit 3e5417bf62101876342cd29dfdf14ead9751a0bb

[fix] `Wide character in subroutine entry` when sending emails
As per https://stackoverflow.com/a/8889290/435004

perllib/formcont_tools.pm
@@ -1,5 +1,6 @@
 package formcont_tools;
 use payonline_tools;
+use Encode;
 
 use vars qw($postURL $redirectURL);
 
@@ -39,8 +40,9 @@ sub send_mail {
   $mail{To}   = $dest;
 
   $mail{Smtp} 	 = 'mail.epfl.ch';
-  $mail{Subject} = $subj;
-  $mail{Message} = $msg;
+  $mail{charset} = "utf-8";
+  $mail{Subject} = Encode::encode('utf8', $subj);
+  $mail{Message} = Encode::encode('utf8', $msg);
 
   $log_event = "formcont::send_mail";
   if (sendmail (%mail)) {

perllib/payonline_tools.pm
@@ -210,8 +210,9 @@ sub send_mail {
   $mail{To}   	 = $dest;
 
   $mail{Smtp} 	 = 'mail.epfl.ch';
-  $mail{Subject} = $subj;
-  $mail{Message} = $msg;
+  $mail{charset} = "utf-8";
+  $mail{Subject} = Encode::encode('utf8', $subj);
+  $mail{Message} = Encode::encode('utf8', $msg);
   if (sendmail (%mail)) {
      if ($Mail::Sendmail::error) {
        log_event($log_event, error => $Mail::Sendmail::error);
@@ -245,8 +246,9 @@ sub send_mail_bc {
   $mail{To}   = $dest;
 
   $mail{Smtp} 	 = 'mail.epfl.ch';
-  $mail{Subject} = $subj;
-  $mail{Message} = $msg;	
+  $mail{charset} = "utf-8";
+  $mail{Subject} = Encode::encode('utf8', $subj);
+  $mail{Message} = Encode::encode('utf8', $msg);
   if (sendmail (%mail)) {
      if ($Mail::Sendmail::error) {
        log_event($log_event, error => $Mail::Sendmail::error);

commit 22d2dc041c000da622a60036eb93fa0db552e725

[fix] Borked e-mails
Broken in 3e5417b — One shouldn't believe everything one reads on the Internet.

perllib/formcont_tools.pm
@@ -40,7 +40,7 @@ sub send_mail {
   $mail{To}   = $dest;
 
   $mail{Smtp} 	 = 'mail.epfl.ch';
-  $mail{charset} = "utf-8";
+  $mail{"Content-Type"} = 'text/plain; charset="utf-8"';
   $mail{Subject} = Encode::encode('utf8', $subj);
   $mail{Message} = Encode::encode('utf8', $msg);
 

perllib/payonline_tools.pm
@@ -210,7 +210,7 @@ sub send_mail {
   $mail{To}   	 = $dest;
 
   $mail{Smtp} 	 = 'mail.epfl.ch';
-  $mail{charset} = "utf-8";
+  $mail{"Content-Type"} = 'text/plain; charset="utf-8"';
   $mail{Subject} = Encode::encode('utf8', $subj);
   $mail{Message} = Encode::encode('utf8', $msg);
   if (sendmail (%mail)) {
@@ -246,7 +246,7 @@ sub send_mail_bc {
   $mail{To}   = $dest;
 
   $mail{Smtp} 	 = 'mail.epfl.ch';
-  $mail{charset} = "utf-8";
+  $mail{"Content-Type"} = 'text/plain; charset="utf-8"';
   $mail{Subject} = Encode::encode('utf8', $subj);
   $mail{Message} = Encode::encode('utf8', $msg);
   if (sendmail (%mail)) {