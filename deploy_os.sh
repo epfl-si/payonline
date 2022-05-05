48615eb0be76004dbfac0254322b53c8aa310e4e
Dominique Quatravaux <dominique.quatravaux@epfl.ch>
2022-05-05 18:38:15 +0200

    [deadcode]

--- deploy_os.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-cd "$(dirname "$0")"
-set -e -x
-
-docker build -t os-docker-registry.epfl.ch/payonline-test/payonline:develop .
-docker push os-docker-registry.epfl.ch/payonline-test/payonline:develop
